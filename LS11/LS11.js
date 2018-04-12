9e6875e6fb7d1557e6f1ca0005c880fb351df1c6
zhaoqianyi1997 <495225376@qq.com>
2018-04-12 20:45:32 +0800

    LS11

---
+++ LS11/LS11.js
@@ -0,0 +1,175 @@
+//立即执行表达式 常见形式
+(function max( x,y){
+    console.log("the max is",x>y?x:y);
+}(2,3));
+
+(function (x,y){ //可以没有函数名
+    console.log("the min is",x<y?x:y);
+})(2,3);
+
+//IIFE是表达式，要注意使用分号结尾，否则可能出现错误
+(function() {
+    console.log("111");
+})();//没有分号的话会报错
+(function () {
+    console.log("222");
+})()
+
+
+
+// 其他形式的IIFE 与运算符结合的写法
+var i = function(){
+    return 10;
+}(); //i为10
+
+
+true && function(a,b){
+    return a>b?a:b;
+}(5,9);
+
+
+!function(x,y){
+    return x==y?true:false; //返回false
+}("5",5);
+//文件内的变量污染问题,尤其是异步执行的情况下
+var userId = 123;
+document.onclick = function () {
+    console.log("userId = ",userId);
+    //alert("userId = "+userId);
+};
+
+
+(function () {
+    var a=2,b=3;
+    if(a<b){
+        var userId = 234;
+    }
+}());
+
+
+
+
+//避免文件之间的全局污染，使用IIFE函数立即执行表达式
+//(function () {
+    var x = 10;
+    document.onclick = function () {
+        // console.log("x = ",x);
+        alert("x = "+x);
+    };
+//})();
+
+//查看Scope窗体中getNumFuncs中每一个函数的内部属性[[Scopes]]中的第0个元素闭包中的变量，看是否存在共享问题
+
+
+
+/*
+//以上代码等价于 存在变量共享问题
+//查看Scope窗体中getNumFuncs中每一个函数的内部属性[[Scopes]]中的第0个元素闭包中的变量，看是否存在共享问题
+function f(){
+    var getNumFuncs = [];//函数数组
+    var i=0;
+    for(;i<10;i++){
+        getNumFuncs[i] = function(){
+            return i;
+        };
+    }
+    return getNumFuncs;//设置断点，查看变量共享问题
+}
+var tmp = f();
+tmp[3]();//tmp[0]()...tmp[9]()都为10
+*/
+
+
+//IIFE 解决变量共享问题
+//查看Scope窗体中getNumFuncs中每一个函数的内部属性[[Scopes]]中的第0个元素闭包中的变量，看是否存在共享问题
+function f(){
+    var getNumFuncs = [];//函数数组
+    for(var i=0;i<10;i++){
+        (function (j) {
+            getNumFuncs[j] = function(){return j;};
+        })(i);
+    }
+    return getNumFuncs;//设置断点，查看变量共享问题
+}
+var tmp = f();
+tmp[3]();//输出为3，tmp[0]()...tmp[9]()都为是期望的结果
+
+
+//局部变量的案例
+function f(){
+    var getNumFuncs = [];//函数数组
+    var j;
+    for(var i=0;i<10;i++){
+        j = i;
+        getNumFuncs[i] = function(){
+            return j;
+        };
+    }
+    return getNumFuncs;//设置断点，查看变量共享问题
+}
+var tmp = f();
+tmp[3]();//tmp[0]()...tmp[9]()都为9
+/避免闭包中非期望的变量共享问题，如下例子
+var tabs = document.getElementsByClassName('tabs')[0].children;
+var contents = document.getElementsByClassName('show')[0];
+
+for(var i=0;i<tabs.length;i++) {
+
+        tabs[i].onclick=function(){
+            for (var j = 0; j < tabs.length; j++) {
+                tabs[j].className = '';
+            }
+            this.className = "active";
+            contents.innerHTML = "导航" + i + "内容";
+        };
+
+}
+
+
+
+//解决方案 IIFE 重新打开index09.html页面测试
+var tabs = document.getElementsByClassName('tabs')[0].children;
+var contents = document.getElementsByClassName('show')[0];
+
+for(var i=0;i<tabs.length;i++) {
+    (function (i) {
+        tabs[i].onclick=function(){
+            for (var j = 0; j < tabs.length; j++) {
+                tabs[j].className = '';
+            }
+            this.className = "active";
+            contents.innerHTML = "导航" + i + "内容";
+        };
+    }(i));
+}
+//变量共享问题
+for (var i = 0; i < 5; i++) {
+    setTimeout(function() {
+        console.log(new Date, i);
+    }, 1000*i);
+}
+//console.log("i：",i);
+*/
+
+
+//通过IIFE解决变量共享问题
+for (var i = 0; i < 5; i++) {
+    (function(j) {  // j = i
+        setTimeout(function() {
+            console.log(new Date, j);
+        }, 1000*i);
+    })(i);
+}
+
+
+//函数作为参数(高阶函数的一种）、静态词法作用域、IIFE
+var max = 10;
+var fn = function (x) {
+    if(x > max){
+        console.log(x);
+    }
+};
+(function (f) {
+    var max = 100;
+    f(15);
+})(fn);
